e2f0e9b853e4c82fc2f7d9e98be97d4c00b23e6e
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-24 11:22:06 +0300

    initial commit

---
+++ src/test/java/com/example/pumb_test_halaiko/PumbTestHalaikoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pumb_test_halaiko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumbTestHalaikoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
