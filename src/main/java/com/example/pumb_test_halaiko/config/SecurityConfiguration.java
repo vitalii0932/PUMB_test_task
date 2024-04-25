e08a68189c486efa5870bfce831791782a9b2b2f
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-25 10:28:29 +0300

    update pom.xml and create securiryConfig class

---
+++ src/main/java/com/example/pumb_test_halaiko/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.pumb_test_halaiko.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
