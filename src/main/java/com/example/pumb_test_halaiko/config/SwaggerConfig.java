2998c54d4b55d1d810de556e0bccdd9e337905fc
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-27 20:08:01 +0300

    create SwaggerConfig class

---
+++ src/main/java/com/example/pumb_test_halaiko/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.example.pumb_test_halaiko.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+}
