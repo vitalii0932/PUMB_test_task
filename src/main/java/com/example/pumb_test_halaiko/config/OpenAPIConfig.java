65d448c73b598323572f706fd418e4f65a90ace8
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-28 18:01:14 +0300

    open api config add task desc in file

--- src/main/java/com/example/pumb_test_halaiko/config/OpenAPIConfig.java
+++ src/main/java/com/example/pumb_test_halaiko/config/OpenAPIConfig.java
@@ -1,17 +1,28 @@
 package com.example.pumb_test_halaiko.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * open API (swagger) configuration class
  */
 @Configuration
+@RequiredArgsConstructor
 public class OpenAPIConfig {
+
+    private final ResourceLoader resourceLoader;
 
     @Bean
     public OpenAPI myOpenAPI() {
@@ -20,29 +31,34 @@ public class OpenAPIConfig {
         contact.setName("Галайко Віталій");
         contact.setUrl("https://t.me/VitaliiGalayko");
 
+        String taskDescription = "";
+
+        try {
+            taskDescription = getTaskDescription("task_description.txt");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         Info info = new Info()
                 .title("Тестове завдання")
                 .version("1.0")
                 .contact(contact)
-                .description("Завдання:\n" +
-                        "1. створити АПІ, з методом POST по шляху files/uploads, який матиме підтримку CSV і XML файлів (приклади файлів з тестовими даними у атачментах)\n" +
-                        "\n" +
-                        "2. замапити об‘єкти з файлів на POJO\n" +
-                        "\n" +
-                        "3. виконати валідацію вхідних об‘єктів, і приймати тільки ті що мають ім‘я, тип, стать, вагу та вартість. Об‘єкти, що не виконують вимогам - скіпаються\n" +
-                        "\n" +
-                        "4. назначити категорії тваринам по вартості\n" +
-                        "\n" +
-                        "5. 0 - 20 - 1ша категорія, 21-40 - 2га, 41-60 - 3тя, від 61 - 4та\n" +
-                        "\n" +
-                        "6. зробити 1 метод GET який буде мати параметри фільтрації по типу тваринки, категорії, статі, а також можливість сортування по будь якому з присутніх в об‘єкті полів та повертати відповідь у json форматі в body\n" +
-                        "\n" +
-                        "7. зробити swagger file з описом API\n" +
-                        "\n" +
-                        "8. покрити юніт тестами\n" +
-                        "\n" +
-                        "9. тип сховища на ваш розсуд, але пам‘ятайте, ще сервіс після рестарту має не втратити об‘єктів");
+                .description(taskDescription);
 
         return new OpenAPI().info(info);
+    }
+
+    private String getTaskDescription(String fileName) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + fileName);
+        InputStream inputStream = resource.getInputStream();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            return content.toString();
+        }
     }
 }
