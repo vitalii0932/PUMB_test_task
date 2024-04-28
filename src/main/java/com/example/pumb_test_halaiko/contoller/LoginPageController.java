4ecebbad9913a1d9230ea34cf8ac8c8b7ea3f0b1
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-28 18:27:12 +0300

    add arrays stream in main page contr

--- src/main/java/com/example/pumb_test_halaiko/contoller/LoginPageController.java
+++ src/main/java/com/example/pumb_test_halaiko/contoller/LoginPageController.java
@@ -2,7 +2,6 @@ package com.example.pumb_test_halaiko.contoller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
