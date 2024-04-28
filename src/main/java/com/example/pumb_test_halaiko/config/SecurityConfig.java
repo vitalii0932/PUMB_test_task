5e8026db45085cb0cdd57ad7b848d69a552cf14c
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-28 18:06:37 +0300

    security config comments

--- src/main/java/com/example/pumb_test_halaiko/config/SecurityConfig.java
+++ src/main/java/com/example/pumb_test_halaiko/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * security configuration
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
